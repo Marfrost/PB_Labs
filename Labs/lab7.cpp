6d0b411a6b67a0a21d49215000aff6962e3c8339
Volodymyr Ryzhkov <waryzhkov@gmail.com>
2020-02-23 17:27:28 +0200

    Dev | Labs 6 and 7 added

---
+++ Labs/lab7.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>;
+using namespace std;
+
+void sortMatrix(int** matrix, int n);
+
+void sortArray(int* array, int n);
+
+void swap(int* a, int* b);
+
+int findLastPositiveRow(int** matrix, int n);
+
+
+int Lab7() {
+	//Рижков ПЗ-19-1, варіант 20
+	int n;
+	cout << "Enter n: ";
+	cin >> n;
+	int** matrix = new int* [n];
+	
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			cout << "matrix[" << i << "][" << j << "]= ";
+			cin >> matrix[i][j];
+		}
+	}
+
+	sortMatrix(matrix, n);
+
+	cout << "Modified matrix: " << endl;
+
+
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << matrix[i][j] << '\t';
+		}
+		cout << endl;
+	}
+
+
+	cout << "Last positive row: " << findLastPositiveRow(matrix, n);
+
+	return 0;
+}
+
+void sortMatrix(int** matrix, int n) {
+	int* inLineMatrix = (int*)malloc(n * n );
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			inLineMatrix[i * n + j] = matrix[i][j];
+
+		}
+	}
+
+
+	sortArray(inLineMatrix, n * n);
+
+
+	for (int d = 0; d < n; d++) {
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (inLineMatrix[d] == matrix[i][j]) {
+					if (i + j != n - 1 || i > d) {
+						swap(matrix[i][j], matrix[d][n - 1 - d]);
+					}
+
+				}
+			}
+		}
+	}
+
+
+
+}
+
+void sortArray(int* array, int n) {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = n - 1; j > i; j--)
+		{
+			if (array[j] > array[j - 1]) {
+				//swap
+				swap(array[j], array[j - 1]);
+
+			}
+		}
+	}
+}
+int findLastPositiveRow(int** matrix, int n) {
+	for (int i = n - 1; i >= 0; i++) {
+		for (int j = 0; j < n; j++)
+		{
+			if (matrix[i][j] < 0) {
+				break;
+			}
+
+			if (j == n - 1) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
--- Labs/main.cpp
+++ Labs/main.cpp
@@ -7,24 +7,38 @@
 #include "lab3.h"
 #include "lab4.h"
 #include "lab5.h"
+#include "lab6.h"
+#include "lab7.h"
 
 using namespace std;
 
 int main() {
 
-	int no=0;
+
+	int semester = 0, lab = 1;
+	cout << "Enter semester: ";
+	cin >> semester;
+
+	if (semester < 1 || semester >2) {
+		cout << "Invalid semester number. Values 1 and 2 are available.";
+	}
+
 	cout << "Enter lab no: ";
-	cin >> no;
+	cin >> lab;
 	
-	switch (no)
+
+
+	switch ((semester-1)*5 + lab)
 	{
 		case 1: Lab1(); break;
 		case 2: Lab2(); break;
 		case 3: Lab3(); break;
 		case 4: Lab4(); break;
 		case 5: Lab5(); break;
+		case 6: Lab6(); break;
+		case 7: Lab7(); break;
 		default: {
-			cout << "Missing lab with no " << no;
+			cout << "Missing lab with no " << lab << " for semester " << semester;
 		}
 		break;
 	}
